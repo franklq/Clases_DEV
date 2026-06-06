75534041886af1ecae6a1697fc421f387f173a9e
Frank Cristian López Quiroz <dt.programador38@upch.pe>
2026-06-05 20:07:50 -0500

    Cambios mcardenasb Clase 1

--- src/CLASES_DEV/Program.cs
+++ src/CLASES_DEV/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Mayra!");
+Console.WriteLine("Ingrese Numero 1:");
+string num1 = Console.ReadLine();
+int edad1=int.Parse(num1);
+
+Console.WriteLine("Ingrese Numero 2:");
+string num2 = Console.ReadLine();
+int edad2=int.Parse(num2);
+
+Console.WriteLine("Ingrese Numero 3:");
+string num3 = Console.ReadLine();
+int edad3=int.Parse(num3);
+
+int respuesta = edad1+edad2+edad3;
+Console.WriteLine("La respuesta es:"+ respuesta);
