091772a4d051b04a923fe0b8dc8942838bf3b80e
Daniela Pérez <danielapj2708@gmail.com>
2026-06-05 20:19:48 -0500

    cambios de daniela

--- src/CLASES_DEV/Program.cs
+++ src/CLASES_DEV/Program.cs
@@ -2,3 +2,29 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World Daniela");
 
+
+Console.Write("Ingrese edad1: ");
+string edad1 = Console.ReadLine();
+int num1=int.Parse(edad1);
+
+Console.Write("Ingrese edad2: ");
+string edad2 = Console.ReadLine();
+int num2= int.Parse(edad2);
+
+
+Console.Write("Ingrese edad3: ");
+string edad3 = Console.ReadLine();
+int num3 = int.Parse(edad3);
+
+int respuesta = num1 + num2 + num3;
+Console.WriteLine("La suma de las edades es: " + respuesta);
+
+
+
+
+
+
+
+
+
+
